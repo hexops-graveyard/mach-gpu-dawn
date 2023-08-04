ce6213fd0ce4fdff3754aeaa9bce7e28d239e30d
Stephen Gutekanst <stephen@hexops.com>
2023-08-03 20:00:15 -0700

    use xcode-frameworks via package manager
    Works around hexops/mach#903
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -64,7 +64,7 @@ pub const Options = struct {
     install_libs: bool = false,
 
     /// The binary release version to use from https://github.com/hexops/mach-gpu-dawn/releases
-            binary_version: []const u8 = "release-4e5d6a5",
+    binary_version: []const u8 = "release-4e5d6a5",
 
     /// Detects the default options to use for the given target.
     pub fn detectDefaults(self: Options, target: std.Target) Options {
@@ -90,17 +90,7 @@ pub fn link(b: *Build, step: *std.build.CompileStep, options: Options) !void {
     const opt = options.detectDefaults(step.target_info.target);
 
     if (step.target_info.target.os.tag == .windows) @import("direct3d_headers").addLibraryPath(step);
-    if (step.target_info.target.os.tag == .macos) {
-        // TODO(build-system): This cannot be imported with the Zig package manager
-        // error: TarUnsupportedFileType
-        //
-        // step.linkLibrary(b.dependency("xcode_frameworks", .{
-        //     .target = step.target,
-        //     .optimize = step.optimize,
-        // }).artifact("xcode-frameworks"));
-        // @import("xcode_frameworks").addPaths(step);
-        xcode_frameworks.addPaths(b, step);
-    }
+    if (step.target_info.target.os.tag == .macos) @import("xcode_frameworks").addPaths(b, step);
 
     try if (options.from_source)
         linkFromSource(b, step, opt)
@@ -577,16 +567,7 @@ fn linkLibDawnCommonDependencies(b: *Build, step: *std.build.CompileStep, option
     _ = options;
     step.linkLibCpp();
     if (step.target_info.target.os.tag == .macos) {
-        // TODO(build-system): This cannot be imported with the Zig package manager
-        // error: TarUnsupportedFileType
-        //
-        // step.linkLibrary(b.dependency("xcode_frameworks", .{
-        //     .target = step.target,
-        //     .optimize = step.optimize,
-        // }).artifact("xcode-frameworks"));
-        // @import("xcode_frameworks").addPaths(step);
-        xcode_frameworks.addPaths(b, step);
-
+        @import("xcode_frameworks").addPaths(b, step);
         step.linkSystemLibraryName("objc");
         step.linkFramework("Foundation");
     }
@@ -737,16 +718,7 @@ fn linkLibDawnNativeDependencies(b: *Build, step: *std.build.CompileStep, option
         }).artifact("direct3d-headers"));
     }
     if (options.metal.?) {
-        // TODO(build-system): This cannot be imported with the Zig package manager
-        // error: TarUnsupportedFileType
-        //
-        // step.linkLibrary(b.dependency("xcode_frameworks", .{
-        //     .target = step.target,
-        //     .optimize = step.optimize,
-        // }).artifact("xcode-frameworks"));
-        // @import("xcode_frameworks").addPaths(step);
-        xcode_frameworks.addPaths(b, step);
-
+        @import("xcode_frameworks").addPaths(b, step);
         step.linkSystemLibraryName("objc");
         step.linkFramework("Metal");
         step.linkFramework("CoreGraphics");
@@ -1278,16 +1250,7 @@ fn linkLibAbseilCppDependencies(b: *Build, step: *std.build.CompileStep, options
     step.linkLibCpp();
     const target = step.target_info.target;
     if (target.os.tag == .macos) {
-        // TODO(build-system): This cannot be imported with the Zig package manager
-        // error: TarUnsupportedFileType
-        //
-        // step.linkLibrary(b.dependency("xcode_frameworks", .{
-        //     .target = step.target,
-        //     .optimize = step.optimize,
-        // }).artifact("xcode-frameworks"));
-        // @import("xcode_frameworks").addPaths(step);
-        xcode_frameworks.addPaths(b, step);
-
+        @import("xcode_frameworks").addPaths(b, step);
         step.linkSystemLibraryName("objc");
         step.linkFramework("CoreFoundation");
     }
@@ -1631,113 +1594,3 @@ fn sdkPath(comptime suffix: []const u8) []const u8 {
         break :blk root_dir ++ suffix;
     };
 }
-
-// TODO(build-system): This is a workaround that we copy anywhere xcode_frameworks needs to be used.
-// With the Zig package manager, it should be possible to remove this entirely and instead just
-// write:
-//
-// ```
-// step.linkLibrary(b.dependency("xcode_frameworks", .{
-//     .target = step.target,
-//     .optimize = step.optimize,
-// }).artifact("xcode-frameworks"));
-// @import("xcode_frameworks").addPaths(step);
-// ```
-//
-// However, today this package cannot be imported with the Zig package manager due to `error: TarUnsupportedFileType`
-// which would be fixed by https://github.com/ziglang/zig/pull/15382 - so instead for now you must
-// copy+paste this struct into your `build.zig` and write:
-//
-// ```
-// try xcode_frameworks.addPaths(b, step);
-// ```
-const xcode_frameworks = struct {
-    pub fn addPaths(b: *std.Build, step: *std.build.CompileStep) void {
-        // branch: mach
-        xEnsureGitRepoCloned(b.allocator, "https://github.com/hexops/xcode-frameworks", "723aa55e9752c8c6c25d3413722b5fe13d72ac4f", xSdkPath("/zig-cache/xcode_frameworks")) catch |err| @panic(@errorName(err));
-
-        step.addFrameworkPath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/Frameworks") });
-        step.addSystemIncludePath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/include") });
-        step.addLibraryPath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/lib") });
-    }
-
-    fn xEnsureGitRepoCloned(allocator: std.mem.Allocator, clone_url: []const u8, revision: []const u8, dir: []const u8) !void {
-        if (xIsEnvVarTruthy(allocator, "NO_ENSURE_SUBMODULES") or xIsEnvVarTruthy(allocator, "NO_ENSURE_GIT")) {
-            return;
-        }
-
-        xEnsureGit(allocator);
-
-        if (std.fs.openDirAbsolute(dir, .{})) |_| {
-            const current_revision = try xGetCurrentGitRevision(allocator, dir);
-            if (!std.mem.eql(u8, current_revision, revision)) {
-                // Reset to the desired revision
-                xExec(allocator, &[_][]const u8{ "git", "fetch" }, dir) catch |err| std.debug.print("warning: failed to 'git fetch' in {s}: {s}\n", .{ dir, @errorName(err) });
-                try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
-                try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
-            }
-            return;
-        } else |err| return switch (err) {
-            error.FileNotFound => {
-                std.log.info("cloning required dependency..\ngit clone {s} {s}..\n", .{ clone_url, dir });
-
-                try xExec(allocator, &[_][]const u8{ "git", "clone", "-c", "core.longpaths=true", clone_url, dir }, ".");
-                try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
-                try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
-                return;
-            },
-            else => err,
-        };
-    }
-
-    fn xExec(allocator: std.mem.Allocator, argv: []const []const u8, cwd: []const u8) !void {
-        var child = std.ChildProcess.init(argv, allocator);
-        child.cwd = cwd;
-        _ = try child.spawnAndWait();
-    }
-
-    fn xGetCurrentGitRevision(allocator: std.mem.Allocator, cwd: []const u8) ![]const u8 {
-        const result = try std.ChildProcess.exec(.{ .allocator = allocator, .argv = &.{ "git", "rev-parse", "HEAD" }, .cwd = cwd });
-        allocator.free(result.stderr);
-        if (result.stdout.len > 0) return result.stdout[0 .. result.stdout.len - 1]; // trim newline
-        return result.stdout;
-    }
-
-    fn xEnsureGit(allocator: std.mem.Allocator) void {
-        const argv = &[_][]const u8{ "git", "--version" };
-        const result = std.ChildProcess.exec(.{
-            .allocator = allocator,
-            .argv = argv,
-            .cwd = ".",
-        }) catch { // e.g. FileNotFound
-            std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
-            std.process.exit(1);
-        };
-        defer {
-            allocator.free(result.stderr);
-            allocator.free(result.stdout);
-        }
-        if (result.term.Exited != 0) {
-            std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
-            std.process.exit(1);
-        }
-    }
-
-    fn xIsEnvVarTruthy(allocator: std.mem.Allocator, name: []const u8) bool {
-        if (std.process.getEnvVarOwned(allocator, name)) |truthy| {
-            defer allocator.free(truthy);
-            if (std.mem.eql(u8, truthy, "true")) return true;
-            return false;
-        } else |_| {
-            return false;
-        }
-    }
-
-    fn xSdkPath(comptime suffix: []const u8) []const u8 {
-        if (suffix[0] != '/') @compileError("suffix must be an absolute path");
-        return comptime blk: {
-            const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-            break :blk root_dir ++ suffix;
-        };
-    }
-};
